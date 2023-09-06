c47eb93654e87642d715f4689044a855c0d17b0b
my name <you@example.com>
2023-09-06 19:53:59 +0200

    chatgpt

--- VLCscript.sh
+++ VLCscript.sh
@@ -2,9 +2,14 @@
 
 sleep 3
 
+vlc vidmorgDELAY.mp4 --no-one-instance &
+sleep 2
 vlc vidmorg.mp4 --no-one-instance &
-sleep 1
-vlc vidmorg.mp4 --no-one-instance &
+
+#for testing :
+sleep 7
+pkill vlc
+#end testing
 
 sleep 35
 
