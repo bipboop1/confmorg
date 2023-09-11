7fa7c4dd7c9c3d5cf01f3d09d300bdf1d3c41238
my name <you@example.com>
2023-09-12 01:48:16 +0200

    jhjhjh

--- wmctrlversion.sh
+++ wmctrlversion.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+echo "launching AI avater morgzbaff..."
+echo "AI intelligenceqd "
 vlc  lettre_x_songood1.mp4 &
 pidL="$!"
 sleep 1
 vlc avatarcadrage_sans_son.mp4 &
 pidA="$!"
 sleep 1
-nameL=$(wmctrl -lp | awk -vpid="$pidL" '$3==pid{print $1}')
-nameA=$(wmctrl -lp | awk -vpid="$pidA" '$3==pid{print $1}')
+nameL=$(wmctrl -lp | awk -vpid="$pidL" '$3==pidL{print $1}')
+nameA=$(wmctrl -lp | awk -vpid="$pidA" '$3==pidA{print $1}')
 wmctrl -ir "$nameA" -e 1,1921,1,1920,1080
 sleep 3
 wmctrl -ir "$nameL" -e 1,1,1,1920,1080
