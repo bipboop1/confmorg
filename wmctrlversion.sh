ea9ccf3343d4119f5ce0bda4709301acd7a1d37b
my name <you@example.com>
2023-09-12 02:58:13 +0200

    we see the light - monday night

--- wmctrlversion.sh
+++ wmctrlversion.sh
@@ -15,17 +15,20 @@ echo "AI Avatar Morgzbaff is now Online."
 sleep 1
 vlc  lettre_x_songood1.mp4 &
 pidL="$!"
-sleep 3
+sleep 5
 vlc avatarcadrage_sans_son.mp4 &
 pidA="$!"
+sleep 5
 nameL=$(wmctrl -lp | awk -vpid="$pidL" '$3==pid{print $1}')
 nameA=$(wmctrl -lp | awk -vpid="$pidA" '$3==pid{print $1}')
-sleep 3
+sleep 5
 wmctrl -ir "$nameA" -e 1,1921,1,1920,1080
-sleep 3
+sleep 5
 wmctrl -ir "$nameL" -e 1,1,1,1920,1080
-sleep 3
-#xdotool key --window "$name" "f"
-sleep 15
+sleep 5
+xdotool key --window "$nameL" "f"
+sleep 5
+xdotool key --window "$nameA" "f"
+sleep 2m
 kill "$pidL" "$pidA"
 exit 0
