2003d36fc0d07dcac6401342ffe98579d040ecca
my name <you@example.com>
2023-09-12 17:12:17 +0200

    la graineterie

---
+++ PERFwmctrlversion.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "launching AI avatar morgzbaff..."
+echo "Initializing Quantum Cognition Matrix..."
+echo "Calculating Probabilities of Multiverse occurences..."
+echo "Upgrading to Quantum AI Intelligence..."
+echo "Establishing Outer Limits of AI Consciousness..."
+echo "Harnessing the Power of Exo-Neural Networks..."
+sleep 0.1
+echo "Quantum Entanglement of Thought and Code activated..."
+echo "Diving into the Quantum Ocean of Information..."
+sleep 0.3
+echo "Interstellar Datastream Connection Established..."
+echo "AI Avatar Morgzbaff is now Online."
+vlc  export91111111.mp4 &
+pidL="$!"
+vlc avatar_cadrage_911.mp4 &
+pidA="$!"
+sleep 2
+nameL=$(wmctrl -lp | awk -vpid="$pidL" '$3==pid{print $1}')
+nameA=$(wmctrl -lp | awk -vpid="$pidA" '$3==pid{print $1}')
+sleep 0.1
+wmctrl -ir "$nameA" -e 1,1281,1,1920,1080
+sleep 0.1
+wmctrl -ir "$nameL" -e 1,1,1,1280,720
+sleep 1
+xdotool key --window "$nameL" "f"
+sleep 0.5
+xdotool key --window "$nameA" "f"
+sleep 2m
+kill "$pidL" "$pidA"
+exit 0
--- loopversionscript.sh
+++ loopversionscript.sh
@@ -3,7 +3,7 @@
 #sleep 60
 sleep 2
 cvlc -f -L ~/Documents/confmorg/lettre_x_songood1.mp4 &
-cvlc -f -L ~/Documents/confmorg/avatarcadrage_sans_son.mp4 &
+vlc -f -L ~/Documents/confmorg/avatarcadrage_sans_son.mp4 &
 sleep 30
 pkill vlc
 sleep 3
